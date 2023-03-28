1e508a3b8fc0c0f9aca3a0b6bd2bc7c25ba15f13
LucasAmaral96 <lucas.a.santos96@hotmail.com>
2023-03-28 18:19:20 -0300

    Security config

--- backend/src/main/java/com/minhasmetas/dsmeta/DsmetaApplication.java
+++ backend/src/main/java/com/minhasmetas/dsmeta/DsmetaApplication.java
@@ -1,4 +1,4 @@
-package com.minhasmetas.dsmeta;
+   package com.minhasmetas.dsmeta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

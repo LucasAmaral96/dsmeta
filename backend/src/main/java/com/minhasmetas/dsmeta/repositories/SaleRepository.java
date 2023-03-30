9d8db15c939f93c34a97da2f11290c3923c75d21
LucasAmaral96 <lucas.a.santos96@hotmail.com>
2023-03-30 16:46:18 -0300

    API test

---
+++ backend/src/main/java/com/minhasmetas/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.minhasmetas.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.minhasmetas.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
